0d493a5bced7e3117280da7caa892b53808b091b
vancasola <616923882@qq.com>
2019-08-08 15:56:53 +0800

    heap

---
+++ DataStructure/heap.cpp
@@ -0,0 +1,88 @@
+//
+//  heap.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2019/8/8.
+//  Copyright © 2019 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <algorithm>
+using namespace std;
+int heap[1000],n;
+void downAdjust(int low,int high)
+{
+    int i=low,j=2*i;
+    while(j<=high)
+    {
+        if(j+1<=high&&heap[j+1]>heap[j])j=j+1;
+        if(heap[j]>heap[i])
+        {
+            swap(heap[i],heap[j]);
+            i=j;
+            j=2*i;
+        }
+        else break;
+    }
+}
+void upAdjust(int low,int high)
+{
+    int i=high,j=i/2;
+    while(j>=low)
+    {
+        if(heap[j]<heap[i])
+        {
+            swap(heap[i],heap[j]);
+            i=j;
+            j=i/2;
+        }
+        else break;
+    }
+}
+void createHeap()
+{
+    for(int i=n/2+1;i>=0;i--)
+        downAdjust(i, n-1);
+}
+void deleteTop()
+{
+    heap[0]=heap[n-1];
+    n--;
+    downAdjust(0, n-1);
+}
+void insert(int x)
+{
+    heap[n++]=x;
+    upAdjust(0, n-1);
+    return;
+}
+void heapsort()
+{
+    createHeap();
+    for(int i=n-1;i>0;i--)
+    {
+        swap(heap[i],heap[0]);
+        downAdjust(0, i-1);
+    }
+}
+int main()
+{
+    n=5;
+    for(int i=0;i<n;i++)scanf("%d",&heap[i]);
+    createHeap();
+    insert(6);
+    heapsort();
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",heap[i]);
+    }
+    putchar('\n');
+    createHeap();
+    for(int i=n-1;i>=0;i--)
+    {
+        printf("%d ",heap[0]);
+        deleteTop();
+    }
+    return 0;
+}
+*/
