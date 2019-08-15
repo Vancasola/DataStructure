39aa456bc6f70ef628af12ef99863fce86fbb4d9
vancasola <616923882@qq.com>
2019-08-15 20:09:01 +0800

    A1003 template

--- DataStructure/A1003.cpp
+++ DataStructure/A1003.cpp
@@ -82,3 +82,100 @@
  return 0;
  }
  */
+
+#include <stdio.h>
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+const int MAXN=510,INF = 0x3fffffff;
+int G[MAXN][MAXN],d[MAXN];
+int n,e,src,dst;
+int u,v,wei;
+int w[MAXN]={false};
+int num[MAXN]={false};
+int weight[MAXN];
+bool vis[MAXN]={false};
+vector<int> pre[MAXN];
+vector<int> pretemp;
+vector<int> ans;
+void dijkstra(int src)
+{
+    fill(vis,vis+MAXN,false);
+    fill(d,d+MAXN,INF);
+    d[src]=0;
+    for(int i=0;i<n;i++)
+    {
+        int u = -1, MIN = INF;
+        for(int j=0;j<n;j++)
+        {
+            if(vis[j]==false&&d[j]<MIN)
+            {
+                MIN = d[j];
+                u=j;
+            }
+        }
+        if(u==-1)return;
+        vis[u]=true;
+        for(int v=0;v<n;v++)
+        {
+            if(vis[v]==false&&G[u][v]!=INF)
+            {
+                if(d[u]+G[u][v]<d[v])
+                {
+                    d[v] = d[u]+G[u][v];
+                    pre[v].clear();
+                    pre[v].push_back(u);
+                }
+                else if(d[u]+G[u][v]==d[v]){
+                    pre[v].push_back(u);
+                }
+            }
+            
+        }
+    }
+    return;
+}
+int MAXV=0;
+void DFS(int v)
+{
+    if(v==src)
+    {
+        pretemp.push_back(v);
+        num[dst]++;
+        ans=pretemp;
+        int value=0;
+        for(vector<int>::iterator it=ans.begin();it!=ans.end();it++)
+        {
+            value+=weight[*it];
+        }
+        if(value>=MAXV){MAXV = value;w[dst] = MAXV;}
+        ans.clear();
+        pretemp.pop_back();
+        return;
+    }
+    pretemp.push_back(v);
+    for(int i=0;i<pre[v].size();i++)
+    {
+        DFS(pre[v][i]);
+    }
+    pretemp.pop_back();
+    
+}
+int main()
+{
+    fill(G[0],G[0]+MAXN*MAXN,INF);
+    cin>>n>>e>>src>>dst;
+    for(int i=0;i<n;i++)cin>>weight[i];
+    for(int i=0;i<e;i++)
+    {
+        cin>>u>>v>>wei;
+        G[u][v]=G[v][u]=wei;
+    }
+    dijkstra(src);
+    //for(int i=0;i<n;i++)
+    //    cout<<d[i]<<endl;
+    DFS(dst);
+    cout<<num[dst]<<' '<<w[dst]<<endl;
+    return 0;
+}
