0673d10311dd63d282f5020e12ea9aac53172fcf
vancasola <616923882@qq.com>
2019-12-31 18:58:15 +0800

    A1031

---
+++ DataStructure/A1031.cpp
@@ -0,0 +1,31 @@
+//
+//  A1031.cpp
+//  DataStructure
+//
+//  Created by vacacasola on 2019/12/31.
+//  Copyright © 2019 none. All rights reserved.
+//  6:33 6:57
+/*
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    int n,i,n1,n2,n3;
+    string s;
+    cin>>s;
+    n1=n3=(s.size()+2)/3;
+    n2=s.size()+2-n1-n3;
+    for( i=0;i<n1-1;i++)
+    {
+        printf("%c",s[i]);
+        for(int j=0;j<n2-2;j++)
+            printf(" ");
+        printf("%c\n",s[s.size()-i-1]);
+    }
+    for(int j=0;j<n2;j++)
+        printf("%c",s[i+j]);
+    return 0;
+}
+*/
