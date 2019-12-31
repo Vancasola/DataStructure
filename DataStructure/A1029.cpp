2797c7cde7455ece782aefd2df11671a914d8162
vancasola <616923882@qq.com>
2019-12-31 18:29:49 +0800

    A1029

---
+++ DataStructure/A1029.cpp
@@ -0,0 +1,36 @@
+//
+//  A1029.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2019/12/31.
+//  Copyright © 2019 none. All rights reserved.
+//  5:58 6:17
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    int n;
+    int t;
+    vector<int> v;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&t);
+        v.push_back(t);
+    }
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&t);
+        v.push_back(t);
+    }
+    sort(v.begin(),v.end());
+    n=v.size();
+    printf("%d",v[(n-1)/2]);
+    return 0;
+}
+*/
