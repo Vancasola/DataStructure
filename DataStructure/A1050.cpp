677691c7f95f959a969ce6cbf6a6398292a200c6
vancasola <616923882@qq.com>
2020-01-02 20:56:58 +0800

    A1050

---
+++ DataStructure/A1050.cpp
@@ -0,0 +1,28 @@
+//
+//  A1049.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/2.
+//  Copyright © 2020 none. All rights reserved.
+//  8:38 8:56
+/*
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    string raw;
+    string dlt;
+    getline(cin,raw);
+    getline(cin,dlt);
+    bool f=false;
+    for(int i=0;i<raw.size();i++)
+    {
+        f=false;
+        for(int j=0;j<dlt.size();j++)if(raw[i]==dlt[j]){f=true;break;}
+        if(!f)putchar(raw[i]);
+    }
+    return 0;
+}
+*/
