57e697e497c025c1ebd2fb5ac1fd959a53dd59f9
vancasola <616923882@qq.com>
2020-01-03 20:01:43 +0800

    A1051

---
+++ DataStructure/A1051.cpp
@@ -0,0 +1,23 @@
+//
+//  A1044.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/3.
+//  Copyright © 2020 none. All rights reserved.
+//  7:45
+/*
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+int main()
+{
+    long long a,b,c;
+    long long g,s,k;
+    scanf("%lld.%lld.%lld",&g,&s,&k);
+    a=g*17*29+s*29+k;
+    scanf("%lld.%lld.%lld",&g,&s,&k);
+    b=g*17*29+s*29+k;
+    c=a+b;
+    printf("%lld.%lld.%lld",c/(17*29),c/29-17*(c/(17*29)),c%29);
+    return 0;
+}*/
