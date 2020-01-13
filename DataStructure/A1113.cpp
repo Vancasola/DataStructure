11f891412c5ebb909ed7f48019f69a16332c6b55
vancasola <616923882@qq.com>
2020-01-13 19:46:28 +0800

    A1113

---
+++ DataStructure/A1113.cpp
@@ -0,0 +1,30 @@
+//
+//  A1113.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/13.
+//  Copyright © 2020 none. All rights reserved.
+//  7:35 7:44
+/*
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main(){
+    vector<int> v(100010);
+    int n;
+    int s1=0,s2=0;
+    int i;
+    cin>>n;
+    for(int i=0;i<n;i++)
+        scanf("%d",&v[i]);
+    sort(v.begin(),v.begin()+n);
+    for( i=0;i<n/2;i++)s1+=v[i];
+    for(;i<n;i++)s2+=v[i];
+    if(n%2==0)cout<<0<<' '<<s2-s1;
+    else cout<<1<<' '<<s2-s1;
+    return 0;
+}
+*/
