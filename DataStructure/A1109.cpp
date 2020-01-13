ef954d00bff645c872c321aceb996699c5cc2cf2
vancasola <616923882@qq.com>
2020-01-13 22:03:17 +0800

    A1109

---
+++ DataStructure/A1109.cpp
@@ -0,0 +1,28 @@
+//
+//  A1109.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/13.
+//  Copyright © 2020 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+using namespace std;
+struct node{
+    string name;
+    int height;
+};
+bool cmp(const node& a,const node& b){
+    if(a.height!=b.height)return a.height<b.height;
+    else return a.name<b.name;
+}
+int main(){
+    
+    return 0;
+}
+*/
