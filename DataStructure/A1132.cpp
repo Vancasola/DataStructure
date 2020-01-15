a27f40f6aee6f1ef953ca2af1f3c17d20d881a68
vancasola <616923882@qq.com>
2020-01-15 20:10:14 +0800

    A1132

---
+++ DataStructure/A1132.cpp
@@ -0,0 +1,33 @@
+//
+//  A1132.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/15.
+//  Copyright © 2020 none. All rights reserved.
+//  7:47 8:09
+/*
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cstdlib>
+using namespace std;
+int main(){
+    int n;
+    string s,sa,sb;
+    long long z,a,b;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        sa=s.substr(0,s.size()/2);
+        sb=s.substr(s.size()/2,s.size());
+        a=stoi(sa);
+        b=stoi(sb);
+        z=a*b;
+        if(z==0){cout<<"No"<<endl;continue;}
+        if(stoi(s)%z==0)cout<<"Yes"<<endl;
+        else cout<<"No"<<endl;
+    }
+    return 0;
+}
+*/
