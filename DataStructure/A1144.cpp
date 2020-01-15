9e13c2e6c38f3929326100042d2fd72147c2162b
vancasola <616923882@qq.com>
2020-01-15 21:11:19 +0800

    A1144

---
+++ DataStructure/A1144.cpp
@@ -0,0 +1,29 @@
+//
+//  A1144.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/1/15.
+//  Copyright © 2020 none. All rights reserved.
+//  9:03 9:10
+/*
+#include <stdio.h>
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    set<int> s;
+    int n,t;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        scanf("%d",&t);
+        if(t<0)continue;
+        s.insert(t);
+    }
+    for(int i=1;;i++)
+        if(s.find(i)==s.end()){
+            printf("%d",i);
+            return 0;
+        }
+    return 0;
+}
+*/
