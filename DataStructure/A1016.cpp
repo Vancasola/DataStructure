b6c8afac6eb6f51580b4d7f15b18ce2626421664
vancasola <616923882@qq.com>
2020-01-25 22:22:20 +0800

    Update A1016.cpp

--- DataStructure/A1016.cpp
+++ DataStructure/A1016.cpp
@@ -4,151 +4,89 @@
 //
 //  Created by vancasola on 2019/12/28.
 //  Copyright © 2019 none. All rights reserved.
-//  10:02 10:15
+//  10:02 10:15 7:43 8:47 10:20
 /*
-#include <stdio.h>
+#include <cstdio>
 #include <iostream>
 #include <string>
 #include <vector>
 #include <algorithm>
 #include <map>
-#include <string.h>
 using namespace std;
-int toll[24]={0};
-struct record
-{
-    string id;
-    int mo,d,h,m;
-    bool f;
-    record(){}
-    record(string s,int mo1,int d1,int h1,int m1,bool f1)
-    {
-        id=s;
-        mo=mo1;
-        d=d1;
-        h=h1;
-        m=m1;
-        f=f1;
-    }
+int toll[24];
+int day=0;
+struct record{
+    string name;
+    int month,dd,hh,mm;
+    string f;
 };
-int fee( record &a, record &b)
-{
-    int sum=0;
-    int t=0;
-    int ad=a.d,ah=a.h,am=a.m;
-    int bd=b.d,bh=b.h,bm=b.m;
-    while(a.d<b.d||a.h<b.h||a.m<b.m)
-    {
-        t++;
-        sum+=toll[a.h];
-        a.m++;
-        if(a.m>=60){
-            a.h++;
-            a.m=0;
-            if(a.h>=24){
-                a.d++;
-                a.h=0;
-            }
-        }
+float count(record a){
+    float fee=0.0;
+    fee=a.dd*day+a.mm*toll[a.hh];
+    for(int i=0;i<a.hh;i++){
+        fee+=toll[i]*60;
     }
-    if(sum){
-    printf("%02d:%02d:%02d %02d:%02d:%02d",ad,ah,am,bd,bh,bm);
-        cout<<' '<<t<<' ';
-        printf("$%.02f\n",(float)sum/100);
-    }
-    return sum;
+    return fee;
 }
-bool cmp1(const record &a,const record &b)
-{
-    return a.m<b.m;
-}
-bool cmp2(const record &a,const record &b)
-{
-    return a.h<b.h;
-}
-bool cmp3(const record &a,const record &b)
-{
-    return a.d<b.d;
-}
-bool cmp4(const record &a,const record &b)
-{
-    return a.mo<b.mo;
-}
-bool cmp5(const string &a,const string &b)
-{
-    return a<b;
+bool cmp(const record& a,const record& b){
+    if(a.name!=b.name)return a.name<b.name;
+    else if(a.dd!=b.dd)return a.dd<b.dd;
+    else if(a.hh!=b.hh)return a.hh<b.hh;
+    else if(a.mm!=b.mm)return a.mm<b.mm;
+    return a.f>b.f;
 }
 int main()
 {
-    for(int i=0;i<24;i++)
-    {
-        scanf("%d",&toll[i]);
-    }
     int n;
+    char name[25],f[10];
+    map<string,vector<record>> mp;
+    record t,a,b;
+    vector<record> v(1010);
+    for(int i=0;i<24;i++){
+        scanf("%d",&toll[i]);
+        day+=toll[i]*60;
+    }
     cin>>n;
-    vector<record> v[1000];
-    vector<string> name;
-    string id,flag;
-    map<string,int> mp;
-    int mo,d,h,m,k,j=-1;
-    bool f;
-    for(int i=0;i<n;i++)
-    {
-        cin>>id;
-        scanf("%d:%d:%d:%d",&mo,&d,&h,&m);
-        cin>>flag;
-        if(flag=="on-line")f=true;
-        else f=false;
-        if(mp.find(id)==mp.end()){
-            mp[id]=++j;
-            name.push_back(id);
-            v[j].push_back(record(id,mo,d,h,m,f));
-        }
-        else
-        {
-            v[mp[id]].push_back(record(id,mo,d,h,m,f));
+    for(int i=0;i<n;i++){
+        scanf("%s %d:%d:%d:%d %s",&name,&v[i].month,&v[i].dd,&v[i].hh,&v[i].mm,&f);
+        v[i].name=name;
+        v[i].f=f;
+    }
+    sort(v.begin(),v.begin()+n,cmp);
+    string last="-";
+    bool flag=false;
+    float total=0;
+    vector<string> r;
+    for(int i=0;i<n-1;i++){
+        a=v[i];
+        b=v[i+1];
+        if(a.name==b.name && a.f=="on-line" && b.f=="off-line"){
+            if(a.name!=last)r.push_back(a.name);
+            last=a.name;
+            mp[a.name].push_back(a);
+            mp[a.name].push_back(b);
         }
     }
-    
-    for(int i=0;i<=j;i++)
-    {
-        sort(v[i].begin(),v[i].end(),cmp1);
-        sort(v[i].begin(),v[i].end(),cmp2);
-        sort(v[i].begin(),v[i].end(),cmp3);
-        sort(v[i].begin(),v[i].end(),cmp4);
-    }
-
-     
-    sort(name.begin(),name.end(),cmp5);
-
-
-    record r1,r2;
-    //fee(v[1][0],v[1][1]);
-    int i=0;
-    for(int g=0;g<=j;g++)
-    {
-        int k=mp[name[g]];
-        int sum=0;
-        bool fb=false;
-        for( i=0;i+1<v[k].size();i++)
-        {
-            if(v[k][i].f&&!v[k][i+1].f)
-            {
-                fb=true;
+    for(int i=0;i<r.size();i++){
+        string x=r[i];
+        if(mp[x].size()<2)continue;
+        else{
+            total=0;
+            float fee,t;
+            a=mp[x][0];
+            printf("%s %02d\n",a.name.c_str(),a.month);
+            for(int j=0;j<mp[x].size();j+=2){
+                a=mp[x][j];
+                b=mp[x][j+1];
+                printf("%02d:%02d:%02d ",a.dd,a.hh,a.mm);
+                printf("%02d:%02d:%02d ",b.dd,b.hh,b.mm);
+                fee=count( b)-count(a);
+                cout<< b.dd*24*60+b.hh*60+b.mm- (a.dd*24*60+a.hh*60+a.mm)<<' ';
+                printf("$%.02f\n",fee/100);
+                total+=fee;
             }
+            printf("Total amount: $%.2f\n",total/100);
         }
-        if(!fb)continue;
-        cout<<v[k][0].id<<' ';
-        printf("%02d\n",v[k][0].mo);
-        for( i=0;i+1<v[k].size();i++)
-        {
-            if(v[k][i].f&&!v[k][i+1].f)
-            {
-                sum+=fee(v[k][i],v[k][i+1]);
-                i++;
-            }
-        }
-        printf("Total amount: $%.2f\n",(float)sum/100);
     }
     return 0;
 }
