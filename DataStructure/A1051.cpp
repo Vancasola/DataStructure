164fc5cd93679810601691d8e2f0ea10bf0d0411
vancasola <616923882@qq.com>
2020-01-27 13:17:04 +0800

    Update A1051.cpp

--- DataStructure/A1051.cpp
+++ DataStructure/A1051.cpp
@@ -1,23 +1,46 @@
 //
-//  A1044.cpp
+//  A1051.cpp
 //  DataStructure
 //
 //  Created by vancasola on 2020/1/3.
 //  Copyright © 2020 none. All rights reserved.
-//  7:45
+//  
 /*
 #include <stdio.h>
 #include <iostream>
+#include <stack>
 using namespace std;
 int main()
 {
-    long long a,b,c;
-    long long g,s,k;
-    scanf("%lld.%lld.%lld",&g,&s,&k);
-    a=g*17*29+s*29+k;
-    scanf("%lld.%lld.%lld",&g,&s,&k);
-    b=g*17*29+s*29+k;
-    c=a+b;
-    printf("%lld.%lld.%lld",c/(17*29),c/29-17*(c/(17*29)),c%29);
+    int m,n,k;
+    int a[1010];
+    cin>>m>>n>>k;
+    for(int i=0;i<k;i++){
+        for(int j=0;j<n;j++)scanf("%d",&a[j]);
+        stack<int> s;
+        bool f=true;
+        int p=1;
+        for(int j=0;j<n;){
+            if(s.empty() || s.top()!=a[j]){
+                s.push(p);
+                p++;
+                if(s.size()>m || p==n+2){
+                    f=false;
+                    break;
+                }
+                if(j==n)break;
+            }
+            if(!f)break;
+            while(!s.empty()&&s.top()==a[j]){
+                s.pop();
+                j++;
+                if(j==n)break;
+            }
+        }
+        if(!s.empty())f=false;
+        if(f)cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
+    }
     return 0;
-}*/
+}
+*/
