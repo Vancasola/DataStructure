861b4cec3435650a836c12fff4b6ac0a8bd2ea74
vancasola <616923882@qq.com>
2020-02-11 12:59:55 +0800

    Update A1006.cpp

--- DataStructure2/A1006.cpp
+++ DataStructure2/A1006.cpp
@@ -4,10 +4,33 @@
 //
 //  Created by vancaosla on 2020/2/11.
 //  Copyright © 2020 none. All rights reserved.
-//
-
+//  12:53 12:59
+/*
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <string>
 using namespace std;
+int main(){
+    int n,hh,mm,ss,t1,t2,Min=99999999,Max=0;
+    string s1,s2,s;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>s;
+        scanf("%d:%d:%d",&hh,&mm,&ss);
+        t1=hh*3600+60*mm+ss;
+        scanf("%d:%d:%d",&hh,&mm,&ss);
+        t2=hh*3600+60*mm+ss;
+        if(t1<Min){
+            s1=s;
+            Min=t1;
+        }
+        if(t2>Max){
+            s2=s;
+            Max=t2;
+        }
+    }
+    cout<<s1<<' '<<s2;
+    return 0;
+}*/
