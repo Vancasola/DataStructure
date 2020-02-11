b02fd38e0ac98207253138aefac1fac5aa2edaca
vancasola <616923882@qq.com>
2020-02-11 12:53:31 +0800

    A1005

---
+++ DataStructure2/A1005.cpp
@@ -0,0 +1,25 @@
+//
+//  A1005.cpp
+//  DataStructure
+//
+//  Created by vancaosla on 2020/2/11.
+//  Copyright © 2020 none. All rights reserved.
+//  12:50 12:53
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+string d[10]={"zero","one","two","three","four","five","six","seven","eight","nine"};
+int main(){
+    string s,str;
+    cin>>s;
+    int sum=0;
+    for(int i=0;i<s.size();i++)sum+=s[i]-'0';
+    str=to_string(sum);
+    cout<<d[str[0]-'0'];
+    for(int i=1;i<str.size();i++)cout<<" "<<d[str[i]-'0'];
+}
+*/
---
+++ DataStructure2/A1006.cpp
@@ -0,0 +1,13 @@
+//
+//  A1006.cpp
+//  DataStructure
+//
+//  Created by vancaosla on 2020/2/11.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
