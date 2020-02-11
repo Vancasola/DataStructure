cfa059b2f626d76ecb4303a89b467e93c906c587
vancasola <616923882@qq.com>
2020-02-11 11:41:27 +0800

    A1004

--- DataStructure2/A1002.cpp
+++ DataStructure2/A1002.cpp
@@ -4,13 +4,46 @@
 //
 //  Created by vancasola on 2020/2/11.
 //  Copyright © 2020 none. All rights reserved.
-//
-
+//  11:13 11:28
+/*
 #include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <unordered_map>
 using namespace std;
-//int main(){
-//    return 0;
-//}
+bool cmp(const pair<int,double> &a,const pair<int,double> &b){
+    return a.first>b.first;
+}
+int main(){
+    unordered_map<int,double> p;
+    int n,m;
+    int e;
+    double c;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        scanf("%d %lf",&e,&c);
+        if(p.find(e)==p.end())p[e]=c;
+        else p[e]+=c;
+    }
+    cin>>n;
+    for(int i=0;i<n;i++){
+        scanf("%d %lf",&e,&c);
+        if(p.find(e)==p.end())p[e]=c;
+        else p[e]+=c;
+    }
+//    for(unordered_map<int, double>::iterator it=p2.begin();it!=p2.end();it++){
+//        p1[it->first]+=it->second;
+//    }
+    vector<pair<int,double>> v;
+    for(unordered_map<int, double>::iterator it=p.begin();it!=p.end();it++){
+        v.push_back(pair<int,double>( it->first,it->second));
+    }
+    sort(v.begin(),v.end(),cmp);
+    printf("%d",v.size());
+    for(int i=0;i<v.size();i++)
+    printf(" %d %.1lf",v[i].first,v[i].second);
+    return 0;
+}
+
+*/
--- DataStructure2/A1004.cpp
+++ DataStructure2/A1004.cpp
@@ -4,11 +4,51 @@
 //
 //  Created by vancasola on 2020/2/11.
 //  Copyright © 2020 none. All rights reserved.
-//
-
+//  11:28 11:41
+/*
 #include <stdio.h>
 #include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
 using namespace std;
-//int main(){
-//    return 0;
-//}
+vector<int> v[105],ans;
+void layerorder(){
+    queue<int> q,p;
+    q.push(1);
+    int x;
+    while (!q.empty()) {
+        p=q;
+        int sum=0;
+        while(!q.empty()){
+            x=q.front();
+            if(!v[x].size())sum++;
+            q.pop();
+        }
+        ans.push_back(sum);
+        while(!p.empty()){
+            x=p.front();
+            p.pop();
+            for(int i=0;i<v[x].size();i++)
+                q.push(v[x][i]);
+        }
+    }
+}
+int main(){
+    int n,m,k,r,x;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        scanf("%d %d",&r,&k);
+        for(int j=0;j<k;j++){
+            scanf("%d",&x);
+            v[r].push_back(x);
+        }
+    }
+    layerorder();
+    for(int i=0;i<ans.size();i++){
+        if(i<ans.size()-1)printf("%d ",ans[i]);
+        else printf("%d",ans[i]);
+    }
+    return 0;
+}
+*/
