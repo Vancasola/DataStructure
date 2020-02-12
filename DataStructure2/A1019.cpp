e54dc3aca46a158ade97ced13ec0f3c7298f0c1a
vancasola <616923882@qq.com>
2020-02-12 21:26:05 +0800

    A1019

---
+++ DataStructure2/A1019.cpp
@@ -0,0 +1,36 @@
+//
+//  A1019.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/2/12.
+//  Copyright © 2020 none. All rights reserved.
+//  9:20 9:25
+/*
+#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+int main(){
+    int n,b;
+    bool f=true;
+    vector<int> v;
+    cin>>n>>b;
+    while(n){
+        v.push_back(n%b);;
+        n/=b;
+    }
+    for(int i=0;i<v.size()/2;i++){
+        if(v[i]!=v[v.size()-1-i]){
+            f=false;
+            break;
+        }
+    }
+    if(f)printf("Yes\n");
+    else printf("No\n");
+    for(int i=v.size()-1;i>0;i--)
+        printf("%d ",v[i]);
+    printf("%d",v[0]);
+}*/
---
+++ DataStructure2/A1021.cpp
@@ -0,0 +1,15 @@
+//
+//  A1021.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/2/12.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+#include <stdio.h>
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
