c6535b3241065f4ebdb0a737a2847b4fc28879e1
vancasola <616923882@qq.com>
2020-02-12 23:46:41 +0800

    Update A1024.cpp

--- DataStructure2/A1024.cpp
+++ DataStructure2/A1024.cpp
@@ -7,24 +7,48 @@
 //  9:25 9:41 10:52 11:20
 /*
 #include <stdio.h>
-#include <stdio.h>
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <string>
+#include <stack>
 using namespace std;
 bool judge(string a){
     for(int i=0;i<a.size()/2;i++){
-        if(a[i]!=a[a.size()-1-i]){
+        if(a[i]!=a[a.size()-1-i])
             return false;
-        }
     }
     return true;
 }
-string rev(string &a){
+string rev(string a){
     string s;
     for(int i=a.size()-1;i>=0;i--)s+=a[i];
     return s;
+}
+string add( string a, string b ){
+    if(b.size()>a.size())swap(a,b);
+    stack<char> s,s2;
+    string c;
+    int r=0;
+    for(int i=0;i<b.size();i++)s.push(b[i]);
+    for(int i=a.size()-1;i>=0;i--){
+        if(s.empty())r=r+a[i]-'0';
+        else {
+            r=r+s.top()+a[i]-2*'0';
+            s.pop();
+        }
+        s2.push(r%10+'0');
+        r/=10;
+    }
+    while(r){
+        s2.push(r%10+'0');
+        r/=10;
+    }
+    while(!s2.empty()){
+        c+=s2.top();
+        s2.pop();
+    }
+    return c;
 }
 int main(){
     long long n,k;
@@ -40,10 +64,11 @@ int main(){
             break;
         }
         else{
-            n=stoll(s1)+stoll(s2);
-            s1=to_string(n);
+            s2=rev(s1);
+            s1=add(s1,s2);
         }
     }
     printf("%s\n%lld",s1.c_str(),p);
     return 0;
-}*/
+}
+*/
