a1187854979050452812ae1090cecd3093b1eb88
vancasola <616923882@qq.com>
2020-02-13 15:07:58 +0800

    Update A1029.cpp

--- DataStructure2/A1029.cpp
+++ DataStructure2/A1029.cpp
@@ -4,8 +4,8 @@
 //
 //  Created by vancasola on 2020/2/13.
 //  Copyright © 2020 none. All rights reserved.
-//  3:00
-
+//  3:00 3:07
+/*
 #include <stdio.h>
 #include <stdio.h>
 #include <iostream>
@@ -14,5 +14,17 @@
 #include <vector>
 using namespace std;
 int main(){
+    int n,m,a[400005];
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    scanf("%d",&m);
+    for(int i=n;i<n+m;i++){
+        scanf("%d",&a[i]);
+    }
+    sort(a,a+n+m);
+    printf("%d",a[(n+m-1)/2]);
     return 0;
 }
+*/
