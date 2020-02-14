363452e259a0c4e52b7bdb7d5c35bd10823e8dac
vancasola <616923882@qq.com>
2020-02-14 17:54:17 +0800

    Update A1039.cpp

--- DataStructure2/A1039.cpp
+++ DataStructure2/A1039.cpp
@@ -4,38 +4,43 @@
 //
 //  Created by vancasola on 2020/2/14.
 //  Copyright © 2020 none. All rights reserved.
-//  4:07 4:22
-/*
-#include <stdio.h>
+//  4:07 4:22 5:22 5:53
+
 #include <stdio.h>
 #include <string>
 #include <vector>
 #include <iostream>
 #include <algorithm>
-#include <map>
+#include <unordered_map>
 using namespace std;
 int main(){
     int n,k,index,m;
-    string s;
-    map<string,vector<int>> p;
+    char s[5];
+    unordered_map<string,vector<int>> mp;
     cin>>n>>k;
     for(int i=0;i<k;i++){
         cin>>index>>m;
         for(int j=0;j<m;j++){
-            cin>>s;
-            p[s].push_back(index);
+            scanf("%s",s);
+            if(mp.find(s)==mp.end()){
+                vector<int> v;
+                mp[s]=v;
+            }
+            mp[s].push_back(index);
         }
     }
-    for(map<string,vector<int>>::iterator it=p.begin();it!=p.end();it++)sort((it->second).begin(),(it->second).end() );
+    
+    for(unordered_map<string,vector<int>>::iterator it=mp.begin();it!=mp.end();it++)
+        sort(it->second.begin(),(it->second).end() );
     for(int i=0;i<n;i++){
         cin>>s;
-        printf("%s %d",s.c_str(),p[s].size());
-        if(!p[s].size()){printf("\n");continue;}
-        for(int j=0;j<p[s].size();j++){
-            printf(" %d",p[s][j]);
+        if(mp.find(s)==mp.end()){printf("%s 0\n",s);continue;}
+        printf("%s %d",s,mp[s].size());
+        for(int j=0;j<mp[s].size();j++){
+            printf(" %d",mp[s][j]);
         }
         printf("\n");
     }
     return 0;
 }
-*/
+
