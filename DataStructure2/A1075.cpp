b71a3ed77e0d2ce67a88f73da81aa76c575e662e
vancasola <616923882@qq.com>
2020-02-21 12:01:26 +0800

    Update A1075.cpp

--- DataStructure2/A1075.cpp
+++ DataStructure2/A1075.cpp
@@ -21,15 +21,17 @@ struct node{
     int s[7];
     bool f=false;
 }a[10005];
+
 bool cmp(const node& a,const node&b){
     return a.sum>b.sum;
 }
 bool cmp2(const node&a,const node& b){
     if(a.f!=b.f)return a.f>b.f;
-    else if(a.r!=b.r)return a.r<b.r;
+    else if(a.sum!=b.sum)return a.sum>b.sum;
     else if(a.cnt!=b.cnt)return a.cnt>b.cnt;
-    else return a.id<a.id;
+    else return a.id<b.id;
 }
+
 int main(){
     cin>>n>>m>>k;
     int id,index,score;
@@ -37,14 +39,13 @@ int main(){
         scanf("%d",&sc[i]);
     }
     for(int i=0;i<=n;i++){
-        fill(a[i].s,a[i].s+7,-1);
+        fill(a[i].s,a[i].s+7,-2);
     }
     for(int i=0;i<k;i++){
         scanf("%d %d %d",&id,&index,&score);
         a[id].id=id;
-        if(score==-1 && a[id].s[index]==-1)a[id].s[index]=0;
-        else if(a[id].s[index]<score){
-            a[id].f=true;
+        if(score!=-1)a[id].f=true;
+        if(a[id].s[index]<score){
             a[id].s[index]=score;
         }
     }
@@ -52,8 +53,9 @@ int main(){
     {
         int cnt=0,sum=0;
         for(int j=1;j<=m;j++){
+            if(a[i].s[j]==-1)a[i].s[j]=0;
             if(a[i].s[j]==sc[j])cnt++;
-            if(a[i].s[j]!=-1)sum+=a[i].s[j];
+            if(a[i].s[j]>0)sum+=a[i].s[j];
         }
         a[i].cnt=cnt;
         a[i].sum=sum;
@@ -67,22 +69,18 @@ int main(){
         }
         a[i].r=r;
     }
+    //for(int i=1;i<=n;i++)printf("%05d %d %d\n",a[i].id,a[i].sum,a[i].r);
     sort(a+1,a+n+1,cmp2);
     for(int i=1;i<=n;i++){
         if(!a[i].f)continue;
         printf("%d %05d %d",a[i].r,a[i].id,a[i].sum);
         for(int j=1;j<=m;j++){
-            if(a[i].s[j]==-1)printf(" -");
+            if(a[i].s[j]==-2)printf(" -");
             else printf(" %d",a[i].s[j]);
         }
         printf("\n");
     }
     return 0;
-}*/
-/*
-1 00002 63 20 25 - 18
-2 00005 42 20 0 22 -
-2 00007 42 - 25 - 17
-2 00001 42 18 18 4 2
-5 00004 40 15 0 25 -
+}
+
 */
