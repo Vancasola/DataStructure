9db9ee700d8ded9aeade29990346b0737ae3e8cc
vancasola <616923882@qq.com>
2020-02-27 20:44:25 +0800

    Update A1136.cpp

--- DataStructure2/A1136.cpp
+++ DataStructure2/A1136.cpp
@@ -17,9 +17,8 @@
 using namespace std;
 bool judge(string s){
     for(int i=0;i<s.size()/2;i++){
-        if(s[i]!=s[s.size()-i-1]){
+        if(s[i]!=s[s.size()-i-1])
             return false;
-        }
     }
     return true;
 }
