53fc2a5b52019b2959510b7262478197366ec237
vancasola <616923882@qq.com>
2020-03-11 15:54:42 +0800

    A1104

---
+++ DataStructure3/A1104.cpp
@@ -0,0 +1,29 @@
+//
+//  A1104.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/3/11.
+//  Copyright © 2020 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n;
+    double t,sum=0.0;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        scanf("%lf",&t);
+        sum+=t*(n-(double)i)*(double)(i+1);
+    }
+    printf("%.2lf",sum);
+    return 0;
+}
+*/
