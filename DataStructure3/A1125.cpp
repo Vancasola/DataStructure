dff213633a0b7c6c310e0863a9807cd3421ec3c7
vancasola <616923882@qq.com>
2020-04-01 18:02:39 +0800

    A1125

---
+++ DataStructure3/A1125.cpp
@@ -0,0 +1,33 @@
+//
+//  A1125.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/4/1.
+//  Copyright © 2020 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int n,t,sum=0;
+    vector<int> v;
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d",&t);
+        v.push_back(t);
+    }
+    sort(v.begin(),v.end());
+    sum=*v.begin();
+    for(int i=1;i<n;i++)
+        sum=(sum+v[i])/2;
+    cout<<sum;
+    return 0;
+}
+*/
