be44e06b8041877aa48a0fbe88cbc9df8725e9bc
vancasola <616923882@qq.com>
2020-04-28 16:37:32 +0800

    A1050

---
+++ DataStructure3/A1050.cpp
@@ -0,0 +1,30 @@
+//
+//  A1050.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/4/28.
+//  Copyright © 2020 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <unordered_map>
+using namespace std;
+int main(){
+    string v,s2;
+    set<char> s;
+    getline(cin,v);
+    getline(cin,s2);
+    for(int i=0;i<s2.size();i++)
+        s.insert(s2[i]);
+    for(int i=0;i<v.size();i++){
+        if(s.find(v[i])==s.end())
+            printf("%c",v[i]);
+    }
+    return 0;
+}
+*/
