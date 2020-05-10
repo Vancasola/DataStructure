0f66130e16f921723861a98876f5d9ee79cb1d8d
vancasola <616923882@qq.com>
2020-05-10 22:44:57 +0800

    A1008

---
+++ DataStructure3/A1008.cpp
@@ -0,0 +1,34 @@
+
+//
+//  A1008.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/5/10.
+//  Copyright © 2020 none. All rights reserved.
+//
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    int n,t,sum=0,p=0;
+    cin>>n;
+    sum=5*n;
+    for(int i=0;i<n;i++){
+        scanf("%d",&t);
+        if(t>p)
+            sum+=6*(t-p);
+        else if(t<p)
+            sum +=4*(p-t);
+        p=t;
+    }
+    cout<<sum;
+    return 0;
+}
+*/
