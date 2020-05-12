6a9ed15fa974fde78f68bbd1c53a7957c5c2061f
vancasola <616923882@qq.com>
2020-05-12 23:11:30 +0800

    A1011

---
+++ DataStructure3/A1011.cpp
@@ -0,0 +1,41 @@
+//
+//  A1011.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/5/12.
+//  Copyright © 2020 none. All rights reserved.
+//  11:01 11:11
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <map>
+using namespace std;
+int main(){
+    double w,t,l,m,r=1;;
+    char a[3];
+    for(int i=0;i<3;i++){
+        scanf("%lf %lf %lf",&w,&t,&l);
+        if(w>t){
+            m=w;
+            a[i]='W';
+        }
+        else{
+            m=t;
+            a[i]='T';
+        }
+        if(m<l){
+            m=l;
+            a[i]='L';
+        }
+        r*=m;
+    }
+    cout<<a[0]<<' '<<a[1]<<' '<<a[2]<<' ';
+    printf("%.2lf",(r*0.65-1)*2);
+    return 0;
+}
+*/
