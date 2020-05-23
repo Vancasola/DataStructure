8c86c86a81b9787805d7d54e3e65093a001f0edf
vancasola <616923882@qq.com>
2020-05-23 22:11:16 +0800

    A1124

---
+++ DataStructure3/A1124.cpp
@@ -0,0 +1,32 @@
+//
+//  A1124.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/5/23.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+#include <stdio.h>
+#include <unordered_map>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <iostream>
+#include <set>
+using namespace std;
+int main(){
+    
+    return 0;
+}
+9 3 2
+Imgonnawin!
+PickMe
+PickMeMeMeee
+LookHere
+Imgonnawin!
+TryAgainAgain
+TryAgainAgain
+Imgonnawin!
+TryAgainAgain
