d2c258b34269b23446075816f50093aedb2891a0
vancasola <616923882@qq.com>
2020-05-24 21:52:14 +0800

    A1054

---
+++ DataStructure3/A1054.cpp
@@ -0,0 +1,32 @@
+//
+//  A1054.cpp
+//  DataStructure
+//
+//  Created by vancasola on 2020/5/24.
+//  Copyright © 2020 none. All rights reserved.
+//  9:47 9:51
+/*
+#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <stack>
+#include <unordered_map>
+using namespace std;
+int main(){
+    int n,m,t;
+    int a=-1,b=0;
+    cin>>n>>m;
+    for(int i=0;i<m;i++){
+        for(int j=0;j<n;j++){
+            scanf("%d",&t);
+            if(a!=t)b--;
+            else b++;
+            if(b<0)a=t;
+        }
+    }
+    printf("%d",a);
+    return 0;
+}
+*/
